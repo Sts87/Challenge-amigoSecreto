20e212c843c820dde2921d70923ca9d86a966e00
Sts87 <a978923505z@gmail.com>
2025-01-16 18:45:35 -0500

    Implementación inicial de la función agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+
+function capturarNombre() {
+    let nombre  = document.getElementById('amigo').value;
+    return nombre
+}
+
+function agregarAmigo() {
+    if (capturarNombre() == '') {
+        alert("Por favor, inserte un nombre");
+    } else {
+        amigos.push(capturarNombre());
+        document.getElementById('amigo').value = '';
+        console.log(amigos)
+    }
+}
