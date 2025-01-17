68333c5fa98fc6592022a0cea8d0899545eeb80a
Sts87 <a978923505z@gmail.com>
2025-01-16 20:01:51 -0500

    Agregación de la función mostrarListaAmigos()

--- app.js
+++ app.js
@@ -14,5 +14,20 @@ function agregarAmigo() {
         amigos.push(capturarNombre());
         document.getElementById('amigo').value = '';
         console.log(amigos)
+        mostrarListaAmigos();
+    }
+}
+
+function mostrarListaAmigos() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = "";
+    
+    for (let i = 0; i < amigos.length; i++) {
+        //Se crea un elemento li
+        let li = document.createElement('li');
+        //Por cada ciclo for adquiere un valor del array amigos
+        li.textContent = amigos[i];
+        //Agrega el valor al elemento
+        lista.appendChild(li);   
     }
 }
